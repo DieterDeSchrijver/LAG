8ea838df76ac507200766f0297afa50a8f840d8f
Dieter De Schrijver <44446698+DieterDeSchrijver@users.noreply.github.com>
2024-03-12 19:42:16 +0100

    updates

--- js/main.js
+++ js/main.js
@@ -68,3 +68,19 @@ System.register(["./scroll.js", "./video-player.js"], function (exports_1, conte
         }
     };
 });
+
+function copy() {
+    // Get the text field
+    console.log("test")
+    var copyText = document.getElementById("numbers");
+  
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+  
+     // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+  
+    // Alert the copied text
+    alert("Copied the text: " + copyText.value);
+  } 
